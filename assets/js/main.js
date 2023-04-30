991ff2c68a3ad56e09114aeeb53e0d962f92e545
freetimezp <kraftukrnet@ukr.net>
2023-04-30 18:22:45 +0300

    05. js-travel-02-mini. Fimal responsive. Slider video bg.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+//show, hide menu
 const menuBtn = document.getElementById("menu-btn");
 const navigation = document.getElementById("navigation");
 
@@ -5,3 +6,32 @@ menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
     navigation.classList.toggle("active");
 });
+
+//slider navigation
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".video-slide");
+const contents = document.querySelectorAll(".content");
+
+var sliderNav = function (manual) {
+    btns.forEach((btn) => {
+        btn.classList.remove("active");
+    })
+
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    })
+
+    contents.forEach((content) => {
+        content.classList.remove("active");
+    })
+
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+    contents[manual].classList.add("active");
+};
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        sliderNav(i);
+    })
+})
